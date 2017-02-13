0d6bda5925d74f98e44a166b61b05dd9a02c10cf
caboose-mobile\caboose <caboosetp@gmail.com>
2017-02-12 18:54:01 -0800

    Project Creation

---
+++ src/ctp/haxerpg/Main.hx
@@ -0,0 +1,22 @@
+package ctp.haxerpg;
+
+import openfl.display.Sprite;
+import openfl.Lib;
+
+/**
+ * ...
+ * @author Garrett "caboosetp" Robertson
+ */
+class Main extends Sprite {
+
+	public function new() {
+		super();
+		
+		// Assets:
+		// openfl.Assets.getBitmapData("img/assetname.jpg");
+		
+		
+		
+	}
+
+}
